1c0a71b96f9dc9d3712f6e0072fea3ccbae71cd7
joeylim <joeylim@cbm.com.sg>
2025-10-21 14:26:01 +0800

    feat: setup three, tailwnd

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="layout" className="h-screen w-screen  bg-white">
+        <div id="canvas" className="h-screen w-screen">
+          <Canvas className="h-full w-full">
+            <ambientLight />
+            <mesh>
+              <boxGeometry />
+              <meshStandardMaterial color="red" />
+            </mesh>
+          </Canvas>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
