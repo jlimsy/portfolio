bb08a69f90797c6b1847c0ad3a50336af5b9773f
jlimsy <limsy.joey@gmail.com>
2025-10-28 00:08:17 +0800

    feat: add click region

--- src/Canvas3D.jsx
+++ src/Canvas3D.jsx
@@ -1,18 +1,8 @@
 import { Canvas } from "@react-three/fiber";
-import {
-  MeshTransmissionMaterial,
-  OrbitControls,
-  Environment,
-  Edges,
-} from "@react-three/drei";
+import { OrbitControls, Environment } from "@react-three/drei";
 import Model3D from "./Model3D";
-import { useControls } from "leva";
-import { gsap } from "gsap";
-import { useEffect, useRef } from "react";
 
 export default function Canvas3D() {
-  const pointerRef = useRef();
-
   // ********** START: Leva GUI **********
 
   // const options = {
@@ -25,46 +15,11 @@ export default function Canvas3D() {
 
   // ********** END: Leva GUI **********
 
-  const handleZoom = () => {
-    console.log("zoom");
-  };
-
-  useEffect(() => {
-    if (!pointerRef.current) return;
-
-    console.log([pointerRef.current]);
-
-    gsap.to(pointerRef.current.material, {
-      opacity: 0,
-      duration: 1,
-      repeat: -1,
-      yoyo: true,
-      ease: "power1.inOut",
-    });
-
-    gsap.to(pointerRef.current.scale, {
-      x: 1.5,
-      y: 1.5,
-      duration: 1,
-      repeat: -1,
-      yoyo: true,
-    });
-  }, [pointerRef.current]);
-
   return (
     <Canvas>
       <ambientLight />
       <Environment preset="sunset" />
-      <mesh
-        position={[0.7, 0.5, 1]}
-        // position={[position.x, position.y, position.z]}
-        onClick={handleZoom}
-        ref={pointerRef}
-      >
-        <circleGeometry args={[0.1, 16]} />
-        <meshStandardMaterial color="white" transparent={true} opacity={0.5} />
-        <Edges color="red" />
-      </mesh>
+
       <Model3D />
       <OrbitControls />
     </Canvas>
--- src/Model3D.jsx
+++ src/Model3D.jsx
@@ -1,5 +1,72 @@
 import { VendingMachine } from "./VendingMachine";
+import * as THREE from "three";
+import { useControls } from "leva";
+import gsap from "gsap";
+import { useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
+import { Edges } from "@react-three/drei";
 
 export default function Model3D() {
-  return <VendingMachine scale={2} />;
+  const pointerRef = useRef();
+  const { camera } = useThree();
+
+  const handleZoom = () => {
+    console.log("zoom");
+
+    const targetPos = new THREE.Vector3();
+
+    if (pointerRef.current) {
+      pointerRef.current.updateWorldMatrix(true, false);
+      pointerRef.current.getWorldPosition(targetPos);
+
+      console.log(pointerRef.current.getWorldPosition(targetPos));
+    }
+    const offset = new THREE.Vector3(0, 0, 1); // 2 units in front of object
+    const newCamPos = targetPos.clone().add(offset);
+
+    gsap.to(camera.position, {
+      x: newCamPos.x,
+      y: newCamPos.y,
+      z: newCamPos.z,
+      duration: 1.5,
+      ease: "power2.out",
+      onUpdate: () => camera.lookAt(targetPos),
+    });
+  };
+
+  useEffect(() => {
+    if (!pointerRef.current) return;
+
+    gsap.to(pointerRef.current.material, {
+      opacity: 0,
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+    });
+
+    gsap.to(pointerRef.current.scale, {
+      x: 1.5,
+      y: 1.5,
+      duration: 1,
+      repeat: -1,
+      yoyo: true,
+    });
+  }, [pointerRef]);
+
+  return (
+    <>
+      <VendingMachine scale={2} />
+      <mesh
+        position={[0.7, 0.5, 1]}
+        // position={[position.x, position.y, position.z]}
+        onClick={handleZoom}
+        ref={pointerRef}
+      >
+        <circleGeometry args={[0.1, 16]} />
+        <meshStandardMaterial color="white" transparent={true} opacity={0.5} />
+        <Edges color="white" />
+      </mesh>
+    </>
+  );
 }
